1e42c5835e513a53efd46ffa665906f3a0e4fa8b
Gideon Okuro <gideonollonde@gmail.com>
2020-11-05 07:27:03 +0300

    add adapter view on itemselected listener

--- app/src/main/java/com/appr/formula6/SignUpActivity.java
+++ app/src/main/java/com/appr/formula6/SignUpActivity.java
@@ -3,12 +3,35 @@ package com.appr.formula6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    @BindView(R.id.userTypeSpinner) Spinner mUserTypeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Spinner spinner = findViewById(R.id.userTypeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cohorts, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter((adapter));
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
