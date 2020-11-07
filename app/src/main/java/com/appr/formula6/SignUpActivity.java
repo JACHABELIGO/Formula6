14048b9b3e4c8d9a5af8d5faa9c44d5a25a873e2
Gideon Okuro <gideonollonde@gmail.com>
2020-11-07 14:23:10 +0300

    bind views and set on click listener

--- app/src/main/java/com/appr/formula6/SignUpActivity.java
+++ app/src/main/java/com/appr/formula6/SignUpActivity.java
@@ -2,16 +2,30 @@ package com.appr.formula6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import butterknife.BindView;
 
-public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.retypePassword) EditText mRetypePassword;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    @BindView(R.id.countrySpinner) Spinner mCountrySpinner;
     @BindView(R.id.userTypeSpinner) Spinner mUserTypeSpinner;
+    @BindView(R.id.phoneEditText) EditText mPhoneEditText;
+    @BindView(R.id.projectEditText) EditText mProjectEditText;
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,9 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter((adapter2));
         spinner2.setOnItemSelectedListener(this);
+
+         mCreateUserButton.setOnClickListener(this);
+         mLoginTextView.setOnClickListener(this);
     }
 
     @Override
@@ -39,5 +56,19 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (view == mCreateUserButton) {
+            createNewUser();
+        }
     }
 }
