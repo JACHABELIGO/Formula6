5b6d67f3417575c9360a2f7c01075b1df41e2fc3
Gideon Okuro <gideonollonde@gmail.com>
2020-11-07 16:59:53 +0300

    declare and set instance for firebase auth

--- app/src/main/java/com/appr/formula6/LoginActivity.java
+++ app/src/main/java/com/appr/formula6/LoginActivity.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
     @BindView(R.id.registerTextView) TextView mRegisterTextView;
+    @BindView(R.id.LoginButton) Button mLoginButton;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         ButterKnife.bind(this);
         mRegisterTextView.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
