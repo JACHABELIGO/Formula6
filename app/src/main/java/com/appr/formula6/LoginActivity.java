d9bda3fa3b15b3aaf17eb6b5e213613b830a474a
Gideon Okuro <gideonollonde@gmail.com>
2020-11-07 13:58:24 +0300

    bind register text view

--- app/src/main/java/com/appr/formula6/LoginActivity.java
+++ app/src/main/java/com/appr/formula6/LoginActivity.java
@@ -3,12 +3,20 @@ package com.appr.formula6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
     }
 }
